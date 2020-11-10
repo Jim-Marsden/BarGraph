16cf0df03791779f885708973d117a89015d4ac0
Jim <jamespmarsden@hotmail.com>
2020-11-10 03:04:54 -0800

    created glfw outline, creating a window, and cleaning up glfw, and the created window..

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <cstdlib>
+#include <GLFW/glfw3.h>
+
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
-  return 0;
+  if(!glfwInit()){
+    std::cout << "Failed to initialize glfw!\n";
+    return EXIT_FAILURE;
+  }
+  else {
+    std::cout << "glfw ready" << std::endl;
+    if(auto const window = glfwCreateWindow(800, 600, "Title", nullptr, nullptr); window){
+      std::cout << "window ready\n";
+      while(!glfwWindowShouldClose(window)){
+        glfwPollEvents();
+      }
+      glfwDestroyWindow(window);
+    }
+    else{
+      std::cout << "window failed to create\n";
+    }
+    glfwTerminate();
+    return EXIT_SUCCESS;
+  }
 }
